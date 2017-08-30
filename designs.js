2e20799aa700b52014b6ec7f776507c877e26896
Unknown <GladwinPrinceC6902@gmail.com>
2017-08-30 21:48:14 +0530

    added grid creation and paint functions

--- designs.js
+++ designs.js
@@ -6,7 +6,21 @@ $("#sizePicker").submit(function(event){
     makeGrid();
 });
 function makeGrid() {
-    console.log("Function triggered")
+    //alert($("#colorPicker").val()+"-"+$("#input_height").val()+"-"+$("#input_width").val());
+    $("#pixel_canvas").empty();
+    for(var i=0, lenrows=$("#input_height").val(); i<lenrows; i++){
+        $("#pixel_canvas").append("<tr class=\"row\"></tr>");
+        for(var j=0, lencols=$("#input_width").val(); j<lencols; j++){
+            $(".row").last().append("<td onclick=\"toggleColor(this)\"></td>");
+        }
+    }
 // Your code goes here!
+}
 
+function toggleColor(pixel){
+    var initcolor=pixel.style.backgroundColor;
+    pixel.style.background=$("#colorPicker").val();
+    if(pixel.style.backgroundColor===initcolor){
+        pixel.style.background="#FFFFFF";
+    }
 }
