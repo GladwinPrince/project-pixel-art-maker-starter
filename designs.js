82eed2ac85c735d5450ab149f760eff7d45fe54a
Unknown <GladwinPrinceC6902@gmail.com>
2017-08-30 18:54:53 +0530

    input parameters declared

--- designs.js
+++ designs.js
@@ -1,6 +1,8 @@
 // Select color input
+const colorPicker=document.getElementById('colorPicker');
 // Select size input
-
+const inputHeight=document.getElementById('input_height');
+const inputWidth=document.getElementById('input_width');
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
