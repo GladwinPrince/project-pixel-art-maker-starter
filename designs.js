18521c8530e570d2e0a8347dce6f1dd07aafed95
Unknown <GladwinPrinceC6902@gmail.com>
2017-08-30 21:47:22 +0530

    Added Event listener to form submit

--- designs.js
+++ designs.js
@@ -1,10 +1,10 @@
 // Select color input
-const colorPicker=document.getElementById('colorPicker');
 // Select size input
-const inputHeight=document.getElementById('input_height');
-const inputWidth=document.getElementById('input_width');
 // When size is submitted by the user, call makeGrid()
-
+$("#sizePicker").submit(function(event){
+    event.preventDefault();
+    makeGrid();
+});
 function makeGrid() {
     console.log("Function triggered")
 // Your code goes here!
