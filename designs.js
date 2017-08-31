661682e46103f40d5f79018659416c47af1a90e0
Unknown <GladwinPrinceC6902@gmail.com>
2017-08-31 09:54:27 +0530

    swapped Jquery functions for JS functions

--- designs.js
+++ designs.js
@@ -1,17 +1,22 @@
 // Select color input
+const colorPicker=document.getElementById("colorPicker");
 // Select size input
+const inputHeight=document.getElementById("input_height");
+const inputWidth=document.getElementById("input_width");
 // When size is submitted by the user, call makeGrid()
-$("#sizePicker").submit(function(event){
+const sizePicker=document.getElementById("sizePicker");
+sizePicker.onsubmit = function(event){
     event.preventDefault();
     makeGrid();
-});
+};
+
+const pixelCanvas=document.getElementById("pixel_canvas");
 function makeGrid() {
-    //alert($("#colorPicker").val()+"-"+$("#input_height").val()+"-"+$("#input_width").val());
-    $("#pixel_canvas").empty();
-    for(var i=0, lenrows=$("#input_height").val(); i<lenrows; i++){
-        $("#pixel_canvas").append("<tr class=\"row\"></tr>");
-        for(var j=0, lencols=$("#input_width").val(); j<lencols; j++){
-            $(".row").last().append("<td onclick=\"toggleColor(this)\"></td>");
+    pixelCanvas.innerHTML="";
+    for(var i=0, lenrows=inputHeight.value; i<lenrows; i++){
+        pixelCanvas.innerHTML+="<tr class=\"row\"></tr>";
+        for(var j=0, lencols=inputWidth.value, row=document.getElementsByClassName("row"); j<lencols; j++){
+            row[row.length-1].innerHTML+="<td onclick=\"toggleColor(this)\"></td>";
         }
     }
 // Your code goes here!
@@ -19,7 +24,7 @@ function makeGrid() {
 
 function toggleColor(pixel){
     var initcolor=pixel.style.backgroundColor;
-    pixel.style.background=$("#colorPicker").val();
+    pixel.style.background=colorPicker.value;
     if(pixel.style.backgroundColor===initcolor){
         pixel.style.background="#FFFFFF";
     }
